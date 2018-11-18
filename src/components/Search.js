e5f6192648a25830a83686de071e4161e70a286e
chimingk <chikwan@ufl.edu>
2018-11-18 01:48:27 -0500

    Updated Search.js
    Search bar updates the list of buildings to view from

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,11 @@ import React from 'react';
 class Search extends React.Component {
 	filterUpdate() {
 		//Here you will need to update the value of the filter with the value from the textbox
+		const val = this.myValue.value
+		this.props.filterUpdate(val)
+
+
+
 	}
 	render() {
 		//You will need to save the value from the textbox and update it as it changes
@@ -10,7 +15,15 @@ class Search extends React.Component {
 		
 		return (
 			<form>
-				<input type="text" placeholder="Type to Filter" />
+
+				<input
+			
+					type="text"
+					placeholder="Type Building Name to Filter" 
+					ref={ value => this.myValue = value }
+					onChange={this.filterUpdate.bind(this)}
+
+				/>
 			</form>
 		);
 	}
